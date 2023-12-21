4744ebf10fee16376e1888da5a8b0a687fbbc658
el-cordero <cordero.elvin@gmail.com>
2023-12-21 14:14:52 +0530

    example commit message

--- Evacuation Time Analysis/03_analysis_run.R
+++ Evacuation Time Analysis/03_analysis_run.R
@@ -92,8 +92,8 @@ for (i in c(1:length(dem.list))){
   file.name <- paste0(path.v,paste0('Pedestrian/',paste0(municipalities[i]),'.shp'))
   writeVector(pa.grid,file.name,overwrite=TRUE)
   endTime <- Sys.time()
-  cat(paste(municipalities,startTime,endTime,sep=','), 
-      file = paste0(municipalities,".txt"))
+  cat(paste(municipalities[i],startTime,endTime,sep=','), 
+      file = paste0(municipalities[i],".txt"))
 }
 
 # # Run on the pa grid function on each municipality
